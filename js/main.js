5dfa51ac563fa20736b374df107a7c6d16d154cb
Sebastian Pieprzyk <pieprzyk.sebastian@gmail.com>
2017-11-28 22:39:06 +0100

    added feature of media section

--- js/main.js
+++ js/main.js
@@ -8,13 +8,39 @@ $(document).ready(function () {
         quote3:{"q":"Zabaw z zespołem Bigiel Band przynosi niezapomniane wrażenia ;p Wszyscy gości weselni świetnie się bawili. Muzyka na wysokim poziomie... nawet największego 'gniotka' zaaranżują, że szczeny opadają ;) POLECAMY SERDECZNIE!!!! Sabina i Mateusz.", "a":"Sabi Naser"},
         quote4:{"q":"W imieniu wesela Anny i Łukasza, dziękujemy za świetną zabawę! Wszyscy Goście jak i Para Młoda bawili się wyśmienicie, wybór utworów a przede wszystkim (!!!) ich wykonania i aranże genialne :) dziękujemy Szymonowi, Sebastianowi, Markowi oraz Krzysztofowi :) ps. Konik na biegunach rządzi !:)", "a":"Jagoda Supernak-Koziarska"},
         quote5:{"q":"(...)Najlepsza kapela na wesele ever! Muzyka świetna, podobała się każdemu (bez przesady - wszyscy goście niezależnie od wieku Was chwalili), zabawy weselne z żartem i ze smakiem, świetna atmosfera a do tego pełny profesjonalizm i dobry humor. Dziękujemy i polecamy wszystkim wesele z Biglami:)))", "a":"Marysia Poloczek"},
-        quote6:{"q":"Serdecznie dziękujemy za rewelacyjną zabawę podczas Naszego przyjęcia weselnego 　 Goście byli zachwyceni... i My też ;D Świetne i w pełni profesjonalne prowadzenie całej imprezy !!! Nieszablonowo, z pomysłem i super humorem, tak potrafi tylko Bigiel Band !!! POLECAMY GORĄCO", "a":"Aneta & Paweł"}
+        quote6:{"q":"Serdecznie dziękujemy za rewelacyjną zabawę podczas Naszego przyjęcia weselnego. Goście byli zachwyceni... i My też ;D Świetne i w pełni profesjonalne prowadzenie całej imprezy !!! Nieszablonowo, z pomysłem i super humorem, tak potrafi tylko Bigiel Band !!! POLECAMY GORĄCO", "a":"Aneta & Paweł"}
         
     };
+    
+    
+    var iframeYT = ["https://www.youtube.com/embed/H4ZTGK7UtxA",
+                    "https://www.youtube.com/embed/JFxS2w69SPk",
+                    "https://www.youtube.com/embed/GqVITtIrlzo",
+                    "https://www.youtube.com/embed/HKPTObTrqik",
+                    "https://www.youtube.com/embed/MMrb-tFJm1I", 
+                    "https://www.youtube.com/embed/uJ7tq54cbMk",
+                    "https://www.youtube.com/embed/wUkXnh_5RsU",
+                    "https://www.youtube.com/embed/aUxw4hSDy8M",
+                    "https://www.youtube.com/embed/yiHCukJpphM",
+                    "https://www.youtube.com/embed/q-31pF4wm5I",
+                    "https://www.youtube.com/embed/1KtgENqxz7g",
+                    "https://www.youtube.com/embed/_PcaghQdbf0",
+                    "https://www.youtube.com/embed/v50HKtFHLdU",
+                    "https://www.youtube.com/embed/vLxognTsvyg",
+                    "https://www.youtube.com/embed/Zsc-CcyShZY",
+                    "https://www.youtube.com/embed/0H8U7mtJiyc",
+                    "https://www.youtube.com/embed/NqJgVS6G_Xk",
+                    "https://www.youtube.com/embed/Gob6mt7f4Is",
+                    "https://www.youtube.com/embed/rC_9OIGu0D4"
+                    
+        
+    ]
         var randomize;
         var rand;
-    
-    
+        var randomize2;
+        var rand2;
+        var iframeYT2 = JSON.parse(JSON.stringify(iframeYT));
+        var elem = 0;
 /////////// function declaration ///////////////    
     
     function randomQuote(){
@@ -104,15 +130,40 @@ $(document).ready(function () {
     }
     
     function fillQuote() {
-       
+       if(!$('.quote1').hasClass('slideEffect')){
+           $('.quote1').addClass('slideEffect');
+       };
        rand = randomQuote();
        $('.quote1').children('p').html(rand["q"]);
        $('.quote1').children('h3').html(rand["a"]);
    }
     
+    function randomVideo (elem) {
+        randomize2 = Math.floor(Math.random()*(19-elem));
+        return randomize2;
+    }
+   
+    function fillVideo () {
+        
+        
+        $('iframe').animate({opacity: '0'}).each(function(){
+            
+            rand2 = randomVideo(elem);
+            $(this).attr('src', iframeYT2[rand2]);
+            iframeYT2.splice(rand2,1);
+            elem++;
+            
+        }).delay(100).animate({opacity: '1'});
+        if (iframeYT2.length<5){
+            iframeYT2 = JSON.parse(JSON.stringify(iframeYT));
+            elem = 0;
+        };
+    };
+    
     ////////// function calls ////
 
-   
+   $('.refresh').on('click', fillVideo);
+    
     fillQuote();
 
     setInterval(fillQuote, 9000);
